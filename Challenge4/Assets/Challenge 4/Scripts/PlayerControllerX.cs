8a8a2ab25324267ec21bc408186ec10a3cb753d5
ldestenay <B00148802@mytudublin.ie>
2021-11-04 15:47:26 +0000

    Particle effects over

--- Challenge4/Assets/Challenge 4/Scripts/PlayerControllerX.cs
+++ Challenge4/Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -27,14 +27,16 @@ public class PlayerControllerX : MonoBehaviour
     {
         // Add force to player in direction of the focal point (and camera)
         float verticalInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime); 
+        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime);
+        boostParticle.transform.position = playerRb.position;
 
         // Set powerup indicator position to beneath player
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
 
         if (Input.GetKeyDown("space"))
         {
-
+            playerRb.AddForce(focalPoint.transform.forward * verticalInput * (speed * 10) * Time.deltaTime);
+            boostParticle.Play();
         }
     }
 
